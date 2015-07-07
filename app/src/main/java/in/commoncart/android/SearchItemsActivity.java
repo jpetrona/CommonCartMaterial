5f1f1b01c68f416b2a956b0a1342407cd2dde557
mikimn <mikimn1999@gmail.com>
2015-07-07 19:54:58 +0300

    First commit, added al screens with a mockup Java frontend

--- app/src/main/java/in/commoncart/android/SearchItemsActivity.java
+++ app/src/main/java/in/commoncart/android/SearchItemsActivity.java
@@ -2,8 +2,8 @@ package in.commoncart.android;
 
 import android.app.Dialog;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,13 +13,18 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 
-public class SearchItemsActivity extends ActionBarActivity {
+public class SearchItemsActivity extends AppCompatActivity {
+
+    private boolean isList;
 
     private ListView mItemsListView;
+    private GridView mItemsGridView;
     private DrawerLayout mDrawerLayout;
 
     private ListView filterSubcategories, filterBrands, filterStores;
@@ -27,6 +32,17 @@ public class SearchItemsActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search_items, menu);
+        return true;
+    }
+
+    private void createList() {
         setContentView(R.layout.activity_search_items);
         mDrawerLayout = (DrawerLayout)findViewById(R.id.activity_search_items_drawer);
         mItemsListView = (ListView)findViewById(R.id.activity_search_items_listview);
@@ -109,7 +125,7 @@ public class SearchItemsActivity extends ActionBarActivity {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                if(convertView == null) {
+                if (convertView == null) {
                     convertView = getLayoutInflater().inflate(R.layout.list_checkbox_item, null);
                 }
                 return convertView;
@@ -119,11 +135,103 @@ public class SearchItemsActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search_items, menu);
-        return true;
+    private void createGrid() {
+        setContentView(R.layout.activity_search_items_gridview);
+        mItemsGridView = (GridView)findViewById(R.id.activity_search_items_gridview);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.activity_search_items_drawer);
+        //Dummy adapter
+        mItemsGridView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 10;
+            }
+
+            @Override
+            public Integer getItem(int position) {
+                return position;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null) {
+                    convertView = getLayoutInflater().inflate(R.layout.product_grid_item, null);
+                }
+                return convertView;
+            }
+        });
+
+        filterSubcategories = (ListView)findViewById(R.id.activity_search_items_filter_subcategories_listview);
+        String[] dummySubs = {"Logitech", "Cyborg", "Razer", "Steelseries"};
+        filterSubcategories.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dummySubs));
+        setListViewHeightBasedOnChildren(filterSubcategories);
+
+        filterBrands = (ListView)findViewById(R.id.activity_search_items_filter_brand_listview);
+        //Dummy adapter
+        filterBrands.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+            @Override
+            public Integer getItem(int position) {
+                return position;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null) {
+                    convertView = getLayoutInflater().inflate(R.layout.list_checkbox_item, null);
+                }
+                return convertView;
+            }
+        });
+        setListViewHeightBasedOnChildren(filterBrands);
+
+        filterStores = (ListView)findViewById(R.id.activity_search_items_filter_store_listview);
+        //Dummy adapter
+        filterStores.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+            @Override
+            public Integer getItem(int position) {
+                return position;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = getLayoutInflater().inflate(R.layout.list_checkbox_item, null);
+                }
+                return convertView;
+            }
+        });
+        setListViewHeightBasedOnChildren(filterStores);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void init(boolean isList) {
+        this.isList = isList;
+        if(isList) createList();
+        else createGrid();
     }
 
     @Override
@@ -145,6 +253,8 @@ public class SearchItemsActivity extends ActionBarActivity {
             return true;
         } else if(id == android.R.id.home) {
             finish();
+        }  else if(id == R.id.action_switch_view) {
+            init(!isList);
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/in/commoncart/android/WishlistActivity.java
+++ app/src/main/java/in/commoncart/android/WishlistActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 
